5001018c441ec6fd39181145a76b5ddf87cbff42
GlavniiLeopold <glavnii_leopold@mail.ru>
2016-11-16 16:51:41 +0300

    [Eltech]HW9

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include <QTextStream>
+#include <QFile>
 #include "parser.h"
 
 int main(int argc, char *argv[])
@@ -8,12 +9,45 @@ int main(int argc, char *argv[])
 
     Parser *parser = new Parser();
     //вывод через дерево tree внутри parser, реализованное через вектор
-    QTextStream stream(stdin);
-    while(1)
+
+    QStringList command_line = a.arguments();
+    if(command_line.size() == 1)
     {
-        std::cout << "\nEnter you sentence \n";
-        parser->parse(stream.readLine());
-        parser->print_tree_nodes();
+        std::cerr << "error - 1 command line arguments needed\n";
+        std::cerr << "trying to read sentences from input stream\n";
+        QTextStream stream(stdin);
+        while(1)
+        {
+            std::cout << "\nEnter you sentence \n";
+            parser->parse(stream.readLine());
+            parser->print_tree_nodes();
+        }
+    }
+    else
+    {
+        QFile inputFile1(command_line.last());
+        if (inputFile1.open(QIODevice::ReadOnly))
+        {
+           QTextStream in(&inputFile1);
+           while (!in.atEnd())
+           {
+               parser->parse(in.readLine());
+               parser->print_tree_nodes();
+           }
+           inputFile1.close();
+        }
+        else
+        {
+            std::cerr << "error, can't find the file at " << command_line.last().toStdString() << "\n";
+            std::cerr << "trying to read sentences from input stream\n";
+            QTextStream stream(stdin);
+            while(1)
+            {
+                std::cout << "\nEnter you sentence \n";
+                parser->parse(stream.readLine());
+                parser->print_tree_nodes();
+            }
+        }
     }
 
     return a.exec();
